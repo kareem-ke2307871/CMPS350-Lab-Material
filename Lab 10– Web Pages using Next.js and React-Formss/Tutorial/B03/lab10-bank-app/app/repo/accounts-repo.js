af03e7297b7a344ad8b50fdfaa1b4a6c4901d774
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-04-09 14:41:33 +0300

    updated

--- Lab 10– Web Pages using Next.js and React-Formss/Tutorial/B03/lab10-bank-app/app/repo/accounts-repo.js
+++ Lab 10– Web Pages using Next.js and React-Formss/Tutorial/B03/lab10-bank-app/app/repo/accounts-repo.js
@@ -18,13 +18,10 @@ class AccountsRepo {
 
     }
     async addAccount(account) {
-        if (account.acctType == 'Savings' || account.acctType == 'Current') {
-            account.balance = 0
-        } else {
-            account.balance = -1000
-        }
 
         account.accountNo = nanoid().slice(0, 4)
+        account.balance = parseInt(account.balance.toString());
+
         const accounts = await this.getAccounts()
         accounts.push(account)
         await fs.writeJSON(this.filePath, accounts)
