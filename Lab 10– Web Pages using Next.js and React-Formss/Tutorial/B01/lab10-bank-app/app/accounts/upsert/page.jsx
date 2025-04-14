69a26d485468652f0096f2041fed92e288341fa9
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-04-14 14:44:04 +0300

    updated

---
+++ Lab 10– Web Pages using Next.js and React-Formss/Tutorial/B01/lab10-bank-app/app/accounts/upsert/page.jsx
@@ -0,0 +1,64 @@
+'use client'
+import React from 'react'
+// import accountsRepo from '@/app/repo/accounts-repo'
+
+export default function AddOrEdit() {
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        const account = Object.fromEntries(formData)
+        const url = `/api/accounts`
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(account)
+        }
+        await fetch(url, options);
+        // navigate to the home page
+
+    }
+
+    return (
+        <>
+            <h3>Add Account</h3>
+            <form id="account-form" onSubmit={handleSubmit}>
+                <label htmlFor="firstname">First Name</label>
+                <input type="text" name="firstname" id="firstname" />
+
+                <label htmlFor="lastname">Last Name</label>
+                <input type="text" name="lastname" id="lastname" />
+
+                <label htmlFor="acctType">Account Type</label>
+                <select name="acctType" id="acctType" required>
+                    <option value=""></option>
+                    <option value="Saving">Saving</option>
+                    <option value="Current">Current</option>
+                </select>
+
+                <label htmlFor="balance">Balance</label>
+                <input type="number" name="balance" id="balance" required />
+
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="email" />
+
+                <label htmlFor="dateOpened">Date Opened</label>
+                <input type="date" name="dateOpened" id="dateOpened" />
+
+                <label htmlFor="gender">Gender</label>
+                <select name="gender" id="gender" required>
+                    <option value=""></option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+
+                </select>
+
+                <label htmlFor="profileImage">Profile Image URL</label>
+                <input type="url" name="profileImage" id="profileImage" />
+
+                <button type="Submit">Submit</button>
+            </form>
+        </>
+    )
+}
