5de3d4a0a339460a27c83d789c600833b1228ba2
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-04-14 15:40:59 +0300

    updated transactions

--- Lab 10– Web Pages using Next.js and React-Formss/Tutorial/B01/lab10-bank-app/app/accounts/trans/add/page.jsx
+++ Lab 10– Web Pages using Next.js and React-Formss/Tutorial/B01/lab10-bank-app/app/accounts/trans/add/page.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+
 
 export default function Transactions() {
+    const [accounts, setAccounts] = useState([])
+    // let accounts = []
+    async function getAccounts() {
+        const response = await fetch('/api/accounts')
+        const data = await response.json()
+        setAccounts(data)
+    }
+
+    useEffect(() => {
+        getAccounts()
+    }, [])
+
+
     return (
-        <div>Add Transaction Page</div>
+        <>
+            <h3>Add Transaction</h3>
+            <form id="trans-form">
+                <label htmlFor="accountNo">Account No</label>
+                <select name="accountNo" id="accountNo" required>
+                    {accounts.map((acct, index) =>
+                        <option key={index}
+                            value={acct.accountNo}>
+                            {acct.accountNo} - {acct.firstname} {acct.lastname} - {acct.balance} QR
+                        </option>
+                    )}
+
+                </select>
+
+                <label htmlFor="type">Transaction Type</label>
+                <select name="transType" id="transType" required>
+                    <option value="Deposit">Deposit</option>
+                    <option value="Withdraw">Withdraw</option>
+                </select>
+
+                <label htmlFor="amount">Amount</label>
+                <input type="number" id="amount" name="amount" required />
+                <button type="Submit">Submit</button>
+            </form>
+        </>
     )
 }
