167291bc5c82acfb91abb0c7205ac531f163f0ad
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-04-23 15:09:29 +0300

    updated

--- Lab 11 – Data Management using Prisma and SQLite Database/Tutorial/B03/lab 11 - banking app using server actions/prisma/migrations/20250423120835_init/migration.sql
+++ Lab 11 – Data Management using Prisma and SQLite Database/Tutorial/B03/lab 11 - banking app using server actions/prisma/migrations/20250423120835_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "account" (
     "accountNo" TEXT NOT NULL PRIMARY KEY,
     "firstname" TEXT NOT NULL,
     "lastname" TEXT NOT NULL,
+    "minimumBalance" REAL,
     "acctType" TEXT NOT NULL,
     "balance" REAL NOT NULL,
     "email" TEXT NOT NULL,
@@ -16,7 +17,7 @@ CREATE TABLE "transaction" (
     "transId" TEXT NOT NULL PRIMARY KEY,
     "transType" TEXT NOT NULL,
     "amount" REAL NOT NULL,
-    "accountBalance" REAL NOT NULL,
+    "accountBalance" REAL,
     "dateCreated" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "accountNo" TEXT NOT NULL,
     CONSTRAINT "transaction_accountNo_fkey" FOREIGN KEY ("accountNo") REFERENCES "account" ("accountNo") ON DELETE RESTRICT ON UPDATE CASCADE
